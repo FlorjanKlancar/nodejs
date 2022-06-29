187b7128be90e28a68042c84d8134281d618a809
FlorjanKlancar <klancar.florjan99@gmail.com>
2022-06-29 09:09:11 +0200

    added user id and sort

--- controllers/statisticsController.js
+++ controllers/statisticsController.js
@@ -2,10 +2,13 @@ import { StatusCodes } from "http-status-codes";
 import Village from "../models/Village.js";
 
 const getStatisticsEndpoint = async (req, res) => {
-  const villageResponse = await Village.find({}).select({
-    population: 1,
-    createdAt: 1,
-  });
+  const villageResponse = await Village.find({})
+    .select({
+      population: 1,
+      createdAt: 1,
+      userId: 1,
+    })
+    .sort({ population: -1 });
 
   if (!villageResponse) {
     throw new NotFoundError("Village not found!");
